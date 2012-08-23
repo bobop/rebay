fd1f01162f94a1934aed7f377a2b79124591969e
Ben Gibbs <ben@bobop.co.uk>
2012-08-23 22:13:46 +0100

    added IncludeSelector values

--- lib/rebay/shopping.rb
+++ lib/rebay/shopping.rb
@@ -117,8 +117,9 @@ module Rebay
     
     private
     def build_request_url(service, params=nil)
-      url = "#{Rebay::Api.base_url}?callname=#{service}&appid=#{Rebay::Api.app_id}&version=#{VERSION}&responseencoding=JSON&siteid=#{Rebay::Api::EBAY_US}"
+      url = "#{Rebay::Api.base_url}?callname=#{service}&appid=#{Rebay::Api.app_id}&version=#{VERSION}&responseencoding=JSON&siteid=#{Rebay::Api::EBAY_US}&IncludeSelector=Variations,ItemSpecifics,HighBidder"
       url += build_rest_payload(params)
+      #Rails.logger.info("--- eBay url = #{url}")
       return url
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,5 @@ require File.join(File.dirname(__FILE__), "..", "lib", "rebay")
 
 Rebay::Api.configure do |rebay|
   rebay.app_id = 'test'
+  rebay.base_url = 'http://open.api.sandbox.ebay.com/shopping'
 end
